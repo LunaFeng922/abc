4555f43be63613da53549701a2620d042895bc36
LunaFeng922 <159014802+LunaFeng922@users.noreply.github.com>
2026-01-24 20:28:22 +0800

    abc-w1-test

--- abc-w1-test/sketch.js
+++ abc-w1-test/sketch.js
@@ -19,17 +19,13 @@ function touchStarted() {
 
 function touchMoved() {
   background(255, 255, 190);
+  textSize(min(width/3, height/3));
+  //textSize(width / touches.length);
+  textAlign(CENTER, CENTER);
   for (let i = 0; i < touches.length; i++) {
     let organ = touchToOrgan[touches[i].id];
     let x = touches[i].x;
     let y = touches[i].y;
-    // let dMax = dist(0, 0, width/2, height/2);
-    // let d = dist(x, y, width/2, height/2);
-    // let n = map(d, 0, dMax,5, 1);
-    // textSize(min(width/3*n, height/3*n));
-    textSize(min(width/3, height/3));
-    //textSize(width / touches.length);
-    textAlign(CENTER, CENTER);
     text(organ,x,y);
   }
   return false;
