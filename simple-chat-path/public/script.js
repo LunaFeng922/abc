1796c5622ef895dd513c81ebbdd0a0a3c6c88b3a
LunaFeng922 <159014802+LunaFeng922@users.noreply.github.com>
2026-02-11 12:00:43 +0800

    change-port

--- simple-chat-path/public/script.js
+++ simple-chat-path/public/script.js
@@ -1,4 +1,10 @@
-let socket = io();
+// let socket = io();
+// socket connection that works locally and on the server:
+if(location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')){
+  socket = io({path: "/luna/port-4240/socket.io"});  // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+}else{
+  socket = io(); 
+}
 
 let formeElm = document.querySelector("#chatForm");
 console.log(formeElm);
