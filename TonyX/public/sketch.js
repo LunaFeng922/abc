89e7b9e8877c21419af21fca567fec6fc66d62c3
LunaFeng922 <159014802+LunaFeng922@users.noreply.github.com>
2026-04-01 14:30:31 +0800

    cc

--- TonyX/public/sketch.js
+++ TonyX/public/sketch.js
@@ -18,6 +18,8 @@ let heroData;
 let traces = {};
 let myTraceID = null;
 let mySocketID = null;
+let myHeadOffsetX = 0;
+let myHeadOffsetY = 0;
 
 let onlinePlayers = {};
 
@@ -127,7 +129,7 @@ function handleNewPosition(pos) {
   if (myOriginLat === null) {
     myOriginLat = currentLat;
     myOriginLon = currentLon;
-    heroData = new ImageData(myOriginLat, myOriginLon, heroImg);
+    heroData = new ImageData(myOriginLat, myOriginLon, heroImg, myHeadOffsetX, myHeadOffsetY);
 
     socket.emit("registerOrigin", {
       originLat: myOriginLat,
@@ -180,15 +182,13 @@ function gpsToScreen(traceData, lat, lon) {
     return null;
 
   let scale = Math.pow(2, myMap.map.getZoom() - zoom);
-  let hx = traceData.headOffsetX * scale;
-  let hy = traceData.headOffsetY * scale;
   let originPx = myMap.latLngToPixel(traceData.originLat, traceData.originLon);
   let pointPx = myMap.latLngToPixel(lat, lon);
   let myOriginPx = myMap.latLngToPixel(myOriginLat, myOriginLon);
 
   return {
-    x: myOriginPx.x + hx + (pointPx.x - originPx.x),
-    y: myOriginPx.y + hy + (pointPx.y - originPx.y),
+    x: myOriginPx.x + (pointPx.x - originPx.x),
+    y: myOriginPx.y + (pointPx.y - originPx.y),
   };
 }
 
@@ -219,6 +219,8 @@ function recalcTrace(traceData) {
 socket.on("connected", function (data) {
   mySocketID = data.socketID;
   myTraceID = data.traceID;
+  myHeadOffsetX = data.headOffsetX;
+  myHeadOffsetY = data.headOffsetY;
 
   for (let id in data.traces) {
     let td = data.traces[id];
@@ -326,10 +328,12 @@ function playerTrace(td) {
 }
 
 class ImageData {
-  constructor(lat, lon, img) {
+  constructor(lat, lon, img, headOffsetX, headOffsetY) {
     this.lat = lat;
     this.lon = lon;
     this.img = img;
+    this.headOffsetX = headOffsetX;
+    this.headOffsetY = headOffsetY;
     this.x = 0;
     this.y = 0;
     this.w = size;
@@ -340,8 +344,8 @@ class ImageData {
     if (!mapInit || !myMap || !myMap.map || myOriginLat === null) return;
     let myOriginPx = myMap.latLngToPixel(myOriginLat, myOriginLon);
     let scale = Math.pow(2, myMap.map.getZoom() - zoom);
-    this.x = myOriginPx.x;
-    this.y = myOriginPx.y;
+    this.x = myOriginPx.x - this.headOffsetX * scale;
+    this.y = myOriginPx.y - this.headOffsetY * scale;
     this.w = size * scale;
     this.h = size * (this.img.height / this.img.width) * scale;
   }
